f121a2bfa2c18740379ad422f612a08dc8a08b4a
Émerson De Pariz <emerson_pariz@hotmail.com>
2022-11-24 22:35:16 -0300

    Agencias adicionadas

--- Main.py
+++ Main.py
@@ -1,25 +1,29 @@
-from classes import ContaBancaria, ContaCorrente, ContaSalario
+from classes import ContaCorrente, ContaSalario, Agencia
 
 
-contas = [
-    ContaBancaria("2387654","0001", 10),
-    ContaBancaria("2387653","0001"),
-    ContaBancaria("2387652","0003", 345.27)
+# contas = [
+#     ContaBancaria("2387654","0001", 10),
+#     ContaBancaria("2387653","0001"),
+#     ContaBancaria("2387652","0003", 345.27)
+# ]
+
+# print(contas[0].get_saldo())
+
+# for conta_bancaria in contas:
+#     print(conta_bancaria)
+
+
+agencias = [
+    Agencia("0001"),
+    Agencia("0049"),
+    Agencia("0003")
 ]
-
-print(contas[0].get_saldo())
-
-for conta_bancaria in contas:
-    print(conta_bancaria)
-
 
 contCor = [
-    ContaCorrente("659845","0001", 5000000),
-    ContaCorrente("659844","0001", 500),
-    ContaCorrente("654895","0044", 45.69)
+    ContaCorrente("659845", agencias[0]),
+    ContaCorrente("659844", agencias[1], 500),
+    ContaCorrente("654895", agencias[2], 45.69)
 ]
-
-#contCor[1].set_saldo(200.00)
 
 print(f"R$ {contCor[1].get_saldo()}")
 
@@ -28,14 +32,13 @@ for conta_corrente in contCor:
 
 
 contSal = [
-    ContaSalario("65695","0001", 4000),
-    ContaSalario("54953","0044", 325.65),
-    ContaSalario("54694","0001", 40.00)
+    ContaSalario("65695",agencias[0], 4000),
+    ContaSalario("54953",agencias[2], 325.65),
+    ContaSalario("54694",agencias[1], 40.00)
 ]
 
 for conta_salario in contSal:
     print(conta_salario)
 
 print(contSal[2].set_saldo(400.00))
-
 
